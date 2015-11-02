c9c2a3613046e2efe7f33a2d6ecf111becfffad8
Andrew Kampjes <andrew.kampjes@powershop.co.nz>
2015-11-02 14:41:38 +1300

    Factor out into class

--- httpserver.rb
+++ httpserver.rb
@@ -3,40 +3,61 @@ require 'socket'
 server = TCPServer.new(8080)
 
 class HTTPRequest
-  attr_reader :type, :filename, :protocol
+  attr_reader :request_type, :filename, :protocol
 
   def initialize(request)
-    @type, @filename, @protocol = request.split
+    @request_type, @filename, @protocol = request.split
 
     # TODO: sanitise and validate filename
     @filename = @filename.gsub(/\A\//, '')
   end
+
+  def build_response
+    response = ""
+
+    begin
+      response = response_headers
+
+      case @request_type
+      when 'HEAD'
+        # Do nothing, HEAD just sends back headers
+      when 'GET'
+        # TODO: read binary file?
+        file =  File.open(@filename, 'r')
+        response += file.read
+      end
+    rescue => e
+      response = "HTTP/1.0 404 Not Found\r\n\r\n"
+    end
+
+    response
+  end
+
+  private
+
+  def response_headers
+    if(File.exists?(@filename))
+      response = "HTTP/1.0 200 OK\r\n"
+      response += "Content-Type: text/plain\r\n"
+      response +=  "\r\n"
+
+      response
+    else
+      fail 'FileNotFound'
+      #"HTTP/1.0 404 Not Found\r\n\r\n"
+    end
+  end
 end
+
 
 loop do
   client = server.accept
   request = client.gets
+  puts request
 
   # GET /testfile.txt HTTP/1.0
   http_request = HTTPRequest.new(request)
-  if(File.exist?(http_request.filename))
-    client.print "HTTP/1.0 200 OK\r\n"
-
-    client.print "Content-Type: text/plain\r\n"
-    client.print "\r\n"
-
-    # TODO: read binary file?
-    file =  File.open(http_request.filename, 'r')
-    output = file.read
-
-    client.print output
-  else
-    client.puts "HTTP/1.0 404 Not Found\r\n"
-    client.print "\r\n"
-  end
-
+  client.print http_request.build_response
   client.close
-
-  puts request
 end
 
